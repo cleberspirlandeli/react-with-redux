0010ecb27b6a53db3e6ba453ae2aa5b572d48792
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-08-15 00:00:21 -0300

    aprendendo redux

---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function toggleLesson(module, lesson) {
+  return {
+    type: "TOGGLE_LESSON",
+    module,
+    lesson
+  };
+}
+
+const Sidebar = ({ modules, dispatch }) => (
+  <aside>
+    {modules.map(module => (
+      <div key={module.id}>
+        <strong>{module.title}</strong>
+        <ul>
+          {module.lessons.map(lesson => (
+            <li key={lesson.id}>
+              {lesson.title}
+              <button onClick={() => dispatch(toggleLesson(module, lesson))}> Selecionar </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </aside>
+);
+
+export default connect(state => ({ modules: state.modules }))(Sidebar);
---
+++ frontend/src/store/index.js
@@ -0,0 +1,42 @@
+import { createStore } from "redux";
+
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "Iniciando com React",
+      lessons: [
+        { id: 1, title: "Primeira aula" },
+        { id: 2, title: "Segunda aula" }
+      ]
+    },
+    {
+      id: 2,
+      title: "Aprendendo Redux",
+      lessons: [
+        { id: 3, title: "Terceira aula" },
+        { id: 4, title: "Quarta aula" }
+      ]
+    }
+  ]
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  console.log(action);
+
+  if (action.type === "TOGGLE_LESSON") {
+    return {
+      ...state,
+      activeLesson: action.lesson,
+      activeModule: action.module
+    };
+  }
+
+  return state;
+}
+
+const store = createStore(reducer);
+
+export default store;
